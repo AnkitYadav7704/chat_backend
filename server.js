bcdaea8e110455cb952daebd962064dec7eb2caf
Ankit Yadav <ankityadav770485@gmail.com>
2025-08-01 00:10:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+const Message = require('./models/Message');
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('send-message', async (data) => {
+    const msg = new Message({ text: data.text, sender: data.sender });
+    await msg.save();
+    io.emit('receive-message', msg); // Send to all
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(5000, () => console.log('Server running on port 5000'));
